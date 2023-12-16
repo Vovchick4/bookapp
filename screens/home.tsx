e32a5dcdeb96164308f391b6b83d9b64cda975eb
Vovchick4 <vovaspodpivas@gmail.com>
2023-12-17 00:13:48 +0200

    agenda

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,7 +1,7 @@
 import moment from "moment";
 import { useState } from "react";
-import { ScrollView, Text, View } from "react-native";
-import Timetable from "react-native-calendar-timetable";
+import { Agenda, AgendaEntry, AgendaSchedule, DateData } from "react-native-calendars";
+import { Text, View } from "react-native";
 
 export const RenderItem = ({ item, dayIndex, daysTotal }: any) => (
     <View style={{
@@ -14,22 +14,80 @@ export const RenderItem = ({ item, dayIndex, daysTotal }: any) => (
     </View>
 )
 
-export default function Home() {
-    const [date] = useState(new Date());
-    const [from] = useState(moment().subtract(3, 'days').toDate());
-    const [till] = useState(moment().add(3, 'days').toDate());
-    const range = { from, till };
-    const [items] = useState([
+const data: AgendaSchedule = {
+    '2023-12-02': [ // Date in 'YYYY-MM-DD' format
         {
-            title: 'Some event',
-            startDate: moment().subtract(1, 'hour').toDate(),
-            endDate: moment().add(1, 'hour').toDate(),
+            name: 'Lunch Appointment',
+            height: 12,
+            day: '2023-12-02T13:20:00', // Date in ISO string format
         },
-    ]);
+        // Add more items for this date if needed
+    ],
+    // Add more dates with their respective items as required
+};
+
+export default function Home() {
+    const [events, setEvents] = useState({})
+
+    const renderItem = (item: AgendaEntry) => {
+        return (
+            <View>
+                <Text>{item.name}</Text>
+                {/* Render other details of the item */}
+            </View>
+        );
+    };
+
+    const renderEmptyDate = () => {
+        return (
+            <View>
+                <Text>This is empty date!</Text>
+            </View>
+        );
+    };
+
+    const loadItems = (day: DateData) => {
+        setTimeout(() => {
+            for (let i = -15; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = timeToString(time);
+
+                if (!events[strTime]) {
+                    events[strTime] = [];
+
+                    const numItems = Math.floor(Math.random() * 3 + 1);
+                    for (let j = 0; j < numItems; j++) {
+                        events[strTime].push({
+                            name: '',
+                            height: Math.max(50, Math.floor(Math.random() * 150)),
+                            day: strTime
+                        });
+                    }
+                }
+            }
+
+            const newItems: AgendaSchedule = {};
+            Object.keys(events).forEach(key => {
+                newItems[key] = events[key];
+            });
+            setEvents(newItems);
+        }, 1000);
+    };
 
     return (
-        <ScrollView>
-            <Timetable items={items} date={date} renderItem={props => <RenderItem {...props} />} />
-        </ScrollView>
+        <View style={{ flex: 1 }}>
+            <Agenda
+                testID="agenda"
+                items={events}
+                loadItemsForMonth={loadItems}
+                renderItem={(item) => renderItem(item)}
+                renderEmptyData={renderEmptyDate}
+            />
+        </View>
     )
+}
+
+function timeToString(dt: number) {
+    const date = new Date(dt);
+    return date.toISOString().split('T')[0]
 }
