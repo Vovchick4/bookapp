28db2d9525d69d42a3d53e3d3205075bfbb3ba85
Vovchick4 <vovaspodpivas@gmail.com>
2024-10-25 20:00:58 +0300

    fixs

--- hooks/use-get-query-user.tsx
+++ hooks/use-get-query-user.tsx
@@ -13,20 +13,24 @@ export default function useGetQueryUser() {
     queryFn: async () => {
       try {
         //const remember_token = await SecureStore.getItemAsync('token');
-        axios.defaults.headers.authorization = `a89JVsT8LDDBfOV18UzsJdeA8va9E5EfFufZTCR14uI4pmUismE2A03HwsSP`;
-        // If token exists, execute the getUser() function to fetch user data
-        const res = await getUser();
-        // console.log(res.data);
-        fillUser(res.data);
-        await queryClient.refetchQueries({ queryKey: ["get-rooms"] });
-        return res.data;
+        if (user?.remember_token) {
+          axios.defaults.headers.authorization = `${user.remember_token}`;
+          // If token exists, execute the getUser() function to fetch user data
+          const res = await getUser();
+          // console.log(res.data);
+          fillUser(res.data);
+          await queryClient.refetchQueries({ queryKey: ["get-rooms"] });
+          return res.data;
+        } else {
+          throw new Error("Logout");
+        }
       } catch (error) {
         // Handle error if getUser() fails
         await signOut();
         throw (error as any).response?.data || { message: "An error occurred" };
       }
     },
-    enabled: !!1, // Enable the query based on token existence
+    enabled: !!user, // Enable the query based on token existence
     refetchOnWindowFocus: false,
   });
 }
