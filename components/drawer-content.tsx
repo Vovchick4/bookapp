afee1a9dfdf67313149f1e3d187362709d601d31
Vovchick4 <vovaspodpivas@gmail.com>
2024-10-28 11:16:12 +0200

    remove subs

--- components/drawer-content.tsx
+++ components/drawer-content.tsx
@@ -7,88 +7,133 @@ import { EUserRole } from "../types/user.entity";
 import { useAppTheme } from "../providers/with-react-paper-ui/with-react-paper-ui";
 
 export default function DrawerContent(props: any) {
-    const { colors } = useAppTheme();
-    const { user, signOut } = useAuth();
+  const { colors } = useAppTheme();
+  const { user, signOut } = useAuth();
 
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                <View style={{ backgroundColor: colors.menuColor }}>
-                    <SafeAreaView style={{ justifyContent: 'center', alignItems: 'center' }}>
-                        <Avatar.Text style={{ backgroundColor: colors.surface }} size={80} label={user?.name || "avatar"} />
-                        <Text style={{ color: colors.surface, marginTop: 20, paddingBottom: 20, fontFamily: 'sans-serif-condensed', fontSize: 17 }}>{`${user?.email}`}</Text>
-                    </SafeAreaView>
-                </View>
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ backgroundColor: colors.menuColor }}>
+          <SafeAreaView
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
+            <Avatar.Text
+              style={{ backgroundColor: colors.surface }}
+              size={80}
+              label={user?.name || "avatar"}
+            />
+            <Text
+              style={{
+                color: colors.surface,
+                marginTop: 20,
+                paddingBottom: 20,
+                fontFamily: "sans-serif-condensed",
+                fontSize: 17,
+              }}
+            >{`${user?.email}`}</Text>
+          </SafeAreaView>
+        </View>
 
-                <Drawer.Item
-                    rippleColor={colors.grayColor}
-                    style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
-                    icon="calendar"
-                    label="Calendar"
-                    onPress={() => props.navigation.navigate("Calendar")}
-                />
+        <Drawer.Item
+          rippleColor={colors.grayColor}
+          style={{
+            backgroundColor: colors.surface,
+            borderRadius: 15,
+            width: "100%",
+            marginLeft: 0,
+          }}
+          icon="calendar"
+          label="Calendar"
+          onPress={() => props.navigation.navigate("Calendar")}
+        />
 
-                <Drawer.Item
-                    rippleColor={colors.grayColor}
-                    style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
-                    icon="account"
-                    label="Profile"
-                    onPress={() => props.navigation.navigate("Profile")}
-                />
+        <Drawer.Item
+          rippleColor={colors.grayColor}
+          style={{
+            backgroundColor: colors.surface,
+            borderRadius: 15,
+            width: "100%",
+            marginLeft: 0,
+          }}
+          icon="account"
+          label="Profile"
+          onPress={() => props.navigation.navigate("Profile")}
+        />
 
-                <Drawer.Item
-                    rippleColor={colors.grayColor}
-                    style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
-                    icon="finance"
-                    label="Finances"
-                    onPress={() => props.navigation.navigate("Finances")}
-                />
+        <Drawer.Item
+          rippleColor={colors.grayColor}
+          style={{
+            backgroundColor: colors.surface,
+            borderRadius: 15,
+            width: "100%",
+            marginLeft: 0,
+          }}
+          icon="finance"
+          label="Finances"
+          onPress={() => props.navigation.navigate("Finances")}
+        />
 
-                {user?.role === EUserRole.owner &&
-                    <Drawer.Item
-                        rippleColor={colors.grayColor}
-                        style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
-                        icon="account-multiple-outline"
-                        label="Employees"
-                        onPress={() => props.navigation.navigate("Employees")}
-                    />}
+        {user?.role === EUserRole.owner && (
+          <Drawer.Item
+            rippleColor={colors.grayColor}
+            style={{
+              backgroundColor: colors.surface,
+              borderRadius: 15,
+              width: "100%",
+              marginLeft: 0,
+            }}
+            icon="account-multiple-outline"
+            label="Employees"
+            onPress={() => props.navigation.navigate("Employees")}
+          />
+        )}
 
-                <Drawer.Item
+        {/* <Drawer.Item
                     rippleColor={colors.grayColor}
                     style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
                     icon="cog"
                     label="Налаштування"
                     onPress={() => props.navigation.navigate("Settings")}
-                />
-            </ScrollView>
+                /> */}
+      </ScrollView>
 
-            <View style={{ borderTopColor: colors.grayColor, borderTopWidth: 1 }}>
-                <Drawer.Item
-                    rippleColor={colors.grayColor}
-                    style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
-                    icon="domain"
-                    label={"Компанія: " + user?.company.name}
-                    onPress={() => props.navigation.navigate("Profile", { tabIndex: 1 })}
-                />
+      <View style={{ borderTopColor: colors.grayColor, borderTopWidth: 1 }}>
+        <Drawer.Item
+          rippleColor={colors.grayColor}
+          style={{
+            backgroundColor: colors.surface,
+            borderRadius: 15,
+            width: "100%",
+            marginLeft: 0,
+          }}
+          icon="domain"
+          label={"Компанія: " + user?.company.name}
+          onPress={() => props.navigation.navigate("Profile", { tabIndex: 1 })}
+        />
 
-                <Drawer.Item
-                    rippleColor={colors.grayColor}
-                    style={{ backgroundColor: colors.surface, borderRadius: 15, width: '100%', marginLeft: 0 }}
-                    icon="exit-to-app"
-                    label="Вийти"
-                    onPress={signOut}
-                />
-            </View>
-        </View>
-    )
+        <Drawer.Item
+          rippleColor={colors.grayColor}
+          style={{
+            backgroundColor: colors.surface,
+            borderRadius: 15,
+            width: "100%",
+            marginLeft: 0,
+          }}
+          icon="exit-to-app"
+          label="Вийти"
+          onPress={signOut}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    text: {
-        fontSize: 25,
-        fontWeight: '500',
-    },
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: "500",
+  },
 });
--- layouts/default.layout.tsx
+++ layouts/default.layout.tsx
@@ -12,159 +12,243 @@ import { DrawerContent, StatusBar } from "../components";
 import CalendarController from "../screens/calendar-controller";
 import { ECalendarViewType, useCalendar } from "../contexts/calendar";
 import { useAppTheme } from "../providers/with-react-paper-ui/with-react-paper-ui";
-import { SubscriptionScreen, CompanyScreen, ProfileScreen, FinancesScreen, EmployeeScreen, SettingsScreen } from "../screens";
+import {
+  SubscriptionScreen,
+  CompanyScreen,
+  ProfileScreen,
+  FinancesScreen,
+  EmployeeScreen,
+  SettingsScreen,
+} from "../screens";
 
-const HomeScreen = lazy(() => import('../screens/home'));
+const HomeScreen = lazy(() => import("../screens/home"));
 // const CalendarControllerScreen = lazy(() => import('../screens/calendar-controller'));
 
 const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function DefaultLayout() {
-    const { colors } = useAppTheme();
-    const { user, signOut } = useAuth();
-    const [visibleMenu, setVisibleMenu] = useState(false);
-    const { queryRoom: { refetch, isLoading, isRefetching }, calendarViewType, currentInterval, openModal, onChangeView } = useCalendar();
-    const spinValue = useRef(new Animated.Value(0)).current;
+  const { colors } = useAppTheme();
+  const { user, signOut } = useAuth();
+  const [visibleMenu, setVisibleMenu] = useState(false);
+  const {
+    queryRoom: { refetch, isLoading, isRefetching },
+    calendarViewType,
+    currentInterval,
+    openModal,
+    onChangeView,
+  } = useCalendar();
+  const spinValue = useRef(new Animated.Value(0)).current;
 
-    useEffect(() => {
-        if (isLoading || isRefetching) {
-            // Start the rotation animation
-            Animated.loop(
-                Animated.timing(spinValue, {
-                    toValue: 1,
-                    duration: 1000,
-                    useNativeDriver: true,
-                })
-            ).start();
-        } else {
-            // Stop the rotation animation
-            spinValue.setValue(0);
-        }
-    }, [isLoading, isRefetching, spinValue]);
+  useEffect(() => {
+    if (isLoading || isRefetching) {
+      // Start the rotation animation
+      Animated.loop(
+        Animated.timing(spinValue, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        })
+      ).start();
+    } else {
+      // Stop the rotation animation
+      spinValue.setValue(0);
+    }
+  }, [isLoading, isRefetching, spinValue]);
 
-    const spin = spinValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: ['0deg', '360deg'],
-    });
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
 
-    return (
-        user?.company ? (
-            <Fragment>
-                <StatusBar backgroundColor={colors.menuColor} />
-                {user.is_subscription || isTrialDate(user.trial_date) ? (
-                    <Drawer.Navigator
-                        drawerContent={(props) => <DrawerContent {...props} />}
-                        screenOptions={{
-                            headerStyle: {
-                                backgroundColor: colors.menuColor // Set your desired background color
-                            },
-                            headerTintColor: colors.surface
-                        }}
-                    >
-                        <Drawer.Screen
-                            name="Calendar"
-                            options={{
-                                headerRight: () => (
-                                    <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 10, columnGap: 20 }}>
-                                        <TouchableOpacity onPress={refetch} disabled={isLoading || isRefetching}>
-                                            <Animated.View style={{ transform: [{ rotate: spin }] }}>
-                                                <MaterialCommunityIcons name="update" size={22} color={(isLoading || isRefetching) ? colors.grayColor : colors.surface} />
-                                            </Animated.View>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity onPress={openModal}>
-                                            <Entypo size={19} color={colors.surface} name="calendar" />
-                                        </TouchableOpacity>
+  return user?.company ? (
+    <Fragment>
+      <StatusBar backgroundColor={colors.menuColor} />
+      {true ? (
+        <Drawer.Navigator
+          drawerContent={(props) => <DrawerContent {...props} />}
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: colors.menuColor, // Set your desired background color
+            },
+            headerTintColor: colors.surface,
+          }}
+        >
+          <Drawer.Screen
+            name="Calendar"
+            options={{
+              headerRight: () => (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: 10,
+                    columnGap: 20,
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={refetch}
+                    disabled={isLoading || isRefetching}
+                  >
+                    <Animated.View style={{ transform: [{ rotate: spin }] }}>
+                      <MaterialCommunityIcons
+                        name="update"
+                        size={22}
+                        color={
+                          isLoading || isRefetching
+                            ? colors.grayColor
+                            : colors.surface
+                        }
+                      />
+                    </Animated.View>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={openModal}>
+                    <Entypo size={19} color={colors.surface} name="calendar" />
+                  </TouchableOpacity>
 
-                                        <Menu
-                                            visible={visibleMenu}
-                                            onDismiss={() => setVisibleMenu(false)}
-                                            anchor={
-                                                <TouchableOpacity onPress={() => setVisibleMenu(true)}>
-                                                    <Entypo size={19} color={colors.surface} name="dots-three-vertical" />
-                                                </TouchableOpacity>
-                                            }>
-                                            <Menu.Item onPress={() => onChangeView(ECalendarViewType.week)} title="Week View" leadingIcon={() => <MaterialCommunityIcons name="calendar-week" size={22} color={calendarViewType === ECalendarViewType.week ? 'red' : 'black'} />} titleStyle={{ color: calendarViewType === ECalendarViewType.week ? 'red' : 'black' }} />
-                                            <Menu.Item onPress={() => onChangeView(ECalendarViewType.month)} title="Month View" leadingIcon={() => <MaterialCommunityIcons name="calendar-month-outline" size={22} color={calendarViewType === ECalendarViewType.month ? 'red' : 'black'} />} titleStyle={{ color: calendarViewType === ECalendarViewType.month ? 'red' : 'black' }} />
-                                        </Menu>
-                                    </View>
-                                )
-                            }}
-                        >
-                            {(props: any) => (
-                                <Suspense fallback={<ActivityIndicator animating={true} color={colors.menuColor} />}>
-                                    <HomeScreen {...props} />
-                                </Suspense>
-                            )}
-                        </Drawer.Screen>
-                        <Drawer.Screen
-                            name="Profile"
-                            component={ProfileScreen}
+                  <Menu
+                    visible={visibleMenu}
+                    onDismiss={() => setVisibleMenu(false)}
+                    anchor={
+                      <TouchableOpacity onPress={() => setVisibleMenu(true)}>
+                        <Entypo
+                          size={19}
+                          color={colors.surface}
+                          name="dots-three-vertical"
                         />
-                        <Drawer.Screen
-                            name="Finances"
-                            component={FinancesScreen}
+                      </TouchableOpacity>
+                    }
+                  >
+                    <Menu.Item
+                      onPress={() => onChangeView(ECalendarViewType.week)}
+                      title="Week View"
+                      leadingIcon={() => (
+                        <MaterialCommunityIcons
+                          name="calendar-week"
+                          size={22}
+                          color={
+                            calendarViewType === ECalendarViewType.week
+                              ? "red"
+                              : "black"
+                          }
                         />
-                        <Drawer.Screen
-                            name="Employees"
-                            component={EmployeeScreen}
-                        />
-                        <Drawer.Screen
-                            name="CalendarController"
-                            options={{
-                                title: "Загрузка..."
-                            }}
-                            component={CalendarController}
-                        />
-                        <Drawer.Screen
-                            name="Settings"
-                            options={{
-                                title: "Налаштування"
-                            }}
-                            component={SettingsScreen}
-                        />
-                    </Drawer.Navigator>
-                ) : (
-                    <Stack.Navigator
-                        screenOptions={{
-                            headerStyle: {
-                                backgroundColor: colors.menuColor // Set your desired background color
-                            },
-                            headerTintColor: colors.surface
-                        }}>
-                        <Stack.Screen
-                            name="Виберіть план"
-                            component={SubscriptionScreen}
-                            options={({ navigation }) => ({
-                                headerRight: () => (
-                                    <IconButton icon="exit-to-app" iconColor={colors.surface} size={28} onPress={signOut} />
-                                )
-                            })}
-                        />
-                    </Stack.Navigator>
-                )}
-            </Fragment>
-        ) : (
-            <Fragment>
-                <StatusBar backgroundColor={colors.menuColor} />
-                <Stack.Navigator
-                    screenOptions={{
-                        headerStyle: {
-                            backgroundColor: colors.menuColor // Set your desired background color
-                        },
-                        headerTintColor: colors.surface
-                    }}>
-                    <Stack.Screen
-                        name="Створити компанію"
-                        component={CompanyScreen}
-                        options={({ navigation }) => ({
-                            headerRight: () => (
-                                <IconButton icon="exit-to-app" iconColor={colors.surface} size={28} onPress={signOut} />
-                            )
-                        })}
+                      )}
+                      titleStyle={{
+                        color:
+                          calendarViewType === ECalendarViewType.week
+                            ? "red"
+                            : "black",
+                      }}
                     />
-                </Stack.Navigator>
-            </Fragment>
-        )
-    )
+                    <Menu.Item
+                      onPress={() => onChangeView(ECalendarViewType.month)}
+                      title="Month View"
+                      leadingIcon={() => (
+                        <MaterialCommunityIcons
+                          name="calendar-month-outline"
+                          size={22}
+                          color={
+                            calendarViewType === ECalendarViewType.month
+                              ? "red"
+                              : "black"
+                          }
+                        />
+                      )}
+                      titleStyle={{
+                        color:
+                          calendarViewType === ECalendarViewType.month
+                            ? "red"
+                            : "black",
+                      }}
+                    />
+                  </Menu>
+                </View>
+              ),
+            }}
+          >
+            {(props: any) => (
+              <Suspense
+                fallback={
+                  <ActivityIndicator
+                    animating={true}
+                    color={colors.menuColor}
+                  />
+                }
+              >
+                <HomeScreen {...props} />
+              </Suspense>
+            )}
+          </Drawer.Screen>
+          <Drawer.Screen name="Profile" component={ProfileScreen} />
+          <Drawer.Screen name="Finances" component={FinancesScreen} />
+          <Drawer.Screen name="Employees" component={EmployeeScreen} />
+          <Drawer.Screen
+            name="CalendarController"
+            options={{
+              title: "Загрузка...",
+            }}
+            component={CalendarController}
+          />
+          {/* <Drawer.Screen
+            name="Settings"
+            options={{
+              title: "Налаштування",
+            }}
+            component={SettingsScreen}
+          /> */}
+        </Drawer.Navigator>
+      ) : (
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: colors.menuColor, // Set your desired background color
+            },
+            headerTintColor: colors.surface,
+          }}
+        >
+          <Stack.Screen
+            name="Виберіть план"
+            component={SubscriptionScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <IconButton
+                  icon="exit-to-app"
+                  iconColor={colors.surface}
+                  size={28}
+                  onPress={signOut}
+                />
+              ),
+            })}
+          />
+        </Stack.Navigator>
+      )}
+    </Fragment>
+  ) : (
+    <Fragment>
+      <StatusBar backgroundColor={colors.menuColor} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.menuColor, // Set your desired background color
+          },
+          headerTintColor: colors.surface,
+        }}
+      >
+        <Stack.Screen
+          name="Створити компанію"
+          component={CompanyScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <IconButton
+                icon="exit-to-app"
+                iconColor={colors.surface}
+                size={28}
+                onPress={signOut}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </Fragment>
+  );
 }
